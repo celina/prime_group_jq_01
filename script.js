1c9112ab4b7b7eaf89aa16033cf44261c4a46026
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-16 16:45:43 -0600

    updated market price function

--- script.js
+++ script.js
@@ -1,77 +1,41 @@
-
-
 var apple = new Fruit('apple', 5, 0);
 var orange = new Fruit('orange', 4, 0);
 var banana = new Fruit('banana', 6, 0);
 var grape = new Fruit('grape', 7, 0);
 
-
-
 var fruitArray = [apple, orange, banana, grape];
 
 $(document).ready(function(){
 
-for (var i = 0; i < fruitArray.length; i++) {
-	var newCell = $('<td><div>' + fruitArray[i].name + '</div><p>' + fruitArray[i].amountPurchased + '</p>' + '<button data-id="' + i + '">buy</button>' +
-	'<p>market price: <span id="market">X</span></p>' +
-	'<p>average price</p>' +
-	'</td>');
+	for (var i = 0; i < fruitArray.length; i++) {
+		var newCell = $('<td><div>' + fruitArray[i].name + '</div><p>' + fruitArray[i].amountPurchased + '</p>' + '<button data-id="' + i + '">buy</button>' +
+		'<p>market price: <span id="market' + i + '">' + fruitArray[i].startingPrice + '</span></p>' +
+		'<p>average price</p>' +
+		'</td>');
+		$('body').append(newCell);
+	}
 
-	// newButton.addClass('button-' + fruitArray[i]);
-	// newButton.data('fruit-' + fruitArray[i]);
-	$('body').append(newCell);
-
-}
-
-
-$('button').on('click', function(){
-	console.log($(this).data('id'));
-	// var fruitName = $(this).data().name;
-	fruitArray[$(this).data('id')].amountPurchased++;
-	$(this).prev().text(fruitArray[$(this).data('id')].amountPurchased);
+	$('button').on('click', function(){
+		console.log($(this).data('id'));
+		// var fruitName = $(this).data().name;
+		fruitArray[$(this).data('id')].amountPurchased++;
+		$(this).prev().text(fruitArray[$(this).data('id')].amountPurchased);
+	});
 });
-
-//then access that objects amount purchased property and increment
-//var fruitPurchased = $(this).data('id');
-//fruitPurchased.amountPurchased + 1;
-//console.log(this);
-});
-
-// for (var i = 0; i < fruitArray.length; i++) {
-// 	var marketPrice = randomNumber(fruitArray[i].startingPrice);
-//
-// }
-
-// });
 
 function Fruit (name, startingPrice, amountPurchased) {
 	this.name = name;
 	this.startingPrice = startingPrice;
 	this.amountPurchased = amountPurchased;
 }
+// left off here - needs ramdomization
+setInterval(function(){
+	for (var i = 0; i < fruitArray.length; i++) {
+	 var updatedPrice = randomMarketPrice(1, 10);
+	 $('#market' + i).html(fruitArray[i].startingPrice);
+ }
+ }, 3000);
 
-// creating market value price function
-
-// setInterval(function marketValuePrice() {
-//
-// })
-
-
-// average price function
-function averagePrice() {
-
-}
-
-
-//finds random market price from .01 - .50 within the limits of .50 - 9.99
-function randomMarketPrice(min, max) {
-	return Math.floor(Math.random() * (1 + max - min) + min).toFixed(2);
-	// console.log('taco');
-}
-
-
-setInterval(function() { $('#market').text('purple'); }, 3000);
-// setInterval(randomMarketPrice(), 15000);
-
-
-// setInterval(randomMarketPrice(1, 1.5), 1000);
+ function randomMarketPrice(min, max) {
+ 	return Math.floor(Math.random() * (1 + max - min) + min).toFixed(2);
+ }
