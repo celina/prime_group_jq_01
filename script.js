cfcde18482d1a6a74e14e965669375e0009d61d0
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-16 16:06:33 -0600

    continued to add code

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 
 
 var apple = new Fruit('apple', 5, 0);
-var orange = new Fruit('orange', 4, 50);
+var orange = new Fruit('orange', 4, 0);
 var banana = new Fruit('banana', 6, 0);
 var grape = new Fruit('grape', 7, 0);
 
-var orangeAmountPurchased = orange.amountPurchased;
-console.log(orangeAmountPurchased);
+
 
 var fruitArray = [apple, orange, banana, grape];
 
 $(document).ready(function(){
 
 for (var i = 0; i < fruitArray.length; i++) {
-	var newCell = $('<td><div>' + fruitArray[i].name + '</div><p data-id' + fruitArray[i].name + '>' + fruitArray[i].amountPurchased + '</p>' + '<button data-id='+ fruitArray[i].name + '>buy</button>' +
-	'<p>market price</p>' +
+	var newCell = $('<td><div>' + fruitArray[i].name + '</div><p>' + fruitArray[i].amountPurchased + '</p>' + '<button data-id="' + i + '">buy</button>' +
+	'<p>market price: <span id="market">X</span></p>' +
 	'<p>average price</p>' +
 	'</td>');
 
@@ -26,15 +25,16 @@ for (var i = 0; i < fruitArray.length; i++) {
 
 
 $('button').on('click', function(){
-var fruitThis = $(this).data('id');
-console.log(fruitThis + ' was clicked');
-if (fruitThis == 'orange'){
-	orangeAmountPurchased = orangeAmountPurchased++;
-}
+	console.log($(this).data('id'));
+	// var fruitName = $(this).data().name;
+	fruitArray[$(this).data('id')].amountPurchased++;
+	$(this).prev().text(fruitArray[$(this).data('id')].amountPurchased);
+});
+
 //then access that objects amount purchased property and increment
 //var fruitPurchased = $(this).data('id');
 //fruitPurchased.amountPurchased + 1;
-console.log(this);
+//console.log(this);
 });
 
 // for (var i = 0; i < fruitArray.length; i++) {
@@ -42,7 +42,7 @@ console.log(this);
 //
 // }
 
-});
+// });
 
 function Fruit (name, startingPrice, amountPurchased) {
 	this.name = name;
@@ -50,10 +50,28 @@ function Fruit (name, startingPrice, amountPurchased) {
 	this.amountPurchased = amountPurchased;
 }
 
+// creating market value price function
+
+// setInterval(function marketValuePrice() {
+//
+// })
+
+
+// average price function
+function averagePrice() {
+
+}
+
+
 //finds random market price from .01 - .50 within the limits of .50 - 9.99
 function randomMarketPrice(min, max) {
-	return Math.floor(Math.random() * (1 + max - min) + min);
- }
+	return Math.floor(Math.random() * (1 + max - min) + min).toFixed(2);
+	// console.log('taco');
+}
 
-var marketPriceChange = randomMarketPrice(.01, .5);
-//console.log(test);
+
+setInterval(function() { $('#market').text('purple'); }, 3000);
+// setInterval(randomMarketPrice(), 15000);
+
+
+// setInterval(randomMarketPrice(1, 1.5), 1000);
